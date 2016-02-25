a2128cefde3f963e062fdf9a06e9e7eea27a5670
Cirych <cir_reg@mail.ru>
2016-02-25 20:12:08 +0300

    ie shims

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,20 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-var bundle = [
+var jsbundle = [
     //'node_modules/es6-shim/es6-shim.min.js',
     'node_modules/systemjs/dist/system-polyfills.js',
+    'node_modules/angular2/es6/dev/src/testing/shims_for_IE.js',
     'node_modules/angular2/bundles/angular2-polyfills.js',
     'node_modules/systemjs/dist/system.src.js',
     'node_modules/rxjs/bundles/Rx.js',
-    'node_modules/angular2/bundles/angular2.dev.js'
+    'node_modules/angular2/bundles/angular2.dev.js',
+];
+var jscopy = [
+    'node_modules/es6-shim/es6-shim.min.js'
+];
+var cssbundle = [
+    'css/style.css'
 ];
 
 gulp.task('lint', function() {
@@ -35,13 +42,22 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('ng2bundle', function() {
-    return gulp.src(bundle)
+gulp.task('js2bundle', function() {
+    return gulp.src(jsbundle)
         .pipe(concat('bundle.js'))
         .pipe(gulp.dest('app'))
         .pipe(rename('bundle.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('app'));
 });
+gulp.task('js2dest', function() {
+    return gulp.src(jscopy)
+        .pipe(gulp.dest('app'));
+});
+gulp.task('css2bundle', function() {
+    return gulp.src(cssbundle)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('app'));
+});
 
-gulp.task('default', ['ng2bundle']);
+gulp.task('default', ['js2dest', 'js2bundle', 'css2bundle']);
